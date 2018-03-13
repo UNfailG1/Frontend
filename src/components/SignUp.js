ee5cfefc8d09f5221802889a0954793a5c3e0226
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-03-13 14:10:15 -0500

    add handle functions

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react'
 
 class SignUp extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      cpass: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit( event ){
+    //axios
+  }
+
+  handleChange( event ){
+    const target = event.target
+    const value = target.value
+    const name = target.id
+
+    this.setState({
+      [name]: value
+    })
+  }
+
   render(){
     return(
       <div className="card-panel white center-align form-panel">
         <h5>Create your personal account.</h5>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="input-field">
-            <label for="username">Username</label>
-            <input id="username" type="text"/>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" onChange={this.handleChange}/>
           </div>
           <div className="input-field ">
-            <label for="email">Email</label>
-            <input id="email" type="email"/>
+            <label htmlFor="email">Email</label>
+            <input id="email" type="email" onChange={this.handleChange}/>
           </div>
           <div className="input-field ">
-            <label  for="password">Password</label>
-            <input id="password" type="password"/>
+            <label  htmlFor="password">Password</label>
+            <input id="password" type="password" onChange={this.handleChange}/>
           </div>
           <div className="input-field">
-            <label for="cpass">Confirm Password</label>
-            <input id="cpass" type="password" />
+            <label htmlFor="cpass">Confirm Password</label>
+            <input id="cpass" type="password" onChange={this.handleChange}/>
           </div>
             <a className="waves-effect waves-light btn primary-color">Sign Up</a>
         </form>
