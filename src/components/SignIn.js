0e4a0c65bbc6cdcd31425a1164069c2419db1088
Raul Coral <rfcoralj@unal.edu.co>
2018-03-13 01:06:37 -0500

    SignIn component

---
+++ src/components/SignIn.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import login_img from '../assets/login_image.jpg'
+
+class SignIn extends Component {
+
+  render(){
+    return (
+      <figure className="back_image">
+        <img src={login_img} alt="The Pulpit Rock"/>
+        <figcaption>
+          <div className="center-align form-panel">
+            <div className="card-panel white">
+              <form>
+              <h5>Sign in to Spairing</h5>
+                <div className="input-field">
+                  <label htmlFor="username">Username or email</label>
+                  <input type="text" id="username"/>
+                </div>
+                <div className="input-field">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" id="password"/>
+                </div>
+                <button className="waves-effect waves-light btn primary-color">Sign In</button>
+              </form>
+            </div>
+            <div className="card-panel blue lighten-5">
+              <span>Are you new on Spairing? <br/><a href="">Create an Account</a></span>
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    )
+  }
+}
+
+export default SignIn
