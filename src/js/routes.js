2aaa4e6d5126b9988f15d594ac32d75f5f3caec5
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-03-14 21:44:26 -0500

    added router schema

---
+++ src/js/routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import App from '../components/App'
+import SignUp from '../components/SignUp'
+import SignIn from '../components/SignIn'
+import Home from '../components/Home'
+
+const Routes = () => (
+  <App>
+    <Switch>
+      <Route path="/" component={ Home }/> */}
+      <Route path="/login" component={ SignIn }/>
+      <Route path="/register" component={ SignUp }/>
+      <Route render={ () => (<h1>404 Not Found</h1>) }/>
+    </Switch>
+  </App>
+)
+
+export default Routes
