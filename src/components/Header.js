f9d140c0caf7bedca09d0a74e9a3f8b0e4c9db2e
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-03-15 00:13:34 -0500

    Added Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
 import { Navbar, NavItem } from 'react-materialize'
-import '../styles/App.css';
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
-    
-    render() {
-    return (
-        
-        <div className="">
-            <Navbar className="Navbar" brand='SPairing Logo' right>
-              <NavItem href='components.html'>Iniciar Sesión</NavItem>
-            </Navbar>
-        </div>
+  initNav(){
+    const $ = window.$
+    $( document ).ready(
+      $(".button-collapse").sideNav()
     )
-    }
+  }
+
+  componentWillMount(){
+    initNav()
+  }
+
+  render() {
+    return (
+      <nav>
+        <div class="nav-wrapper primary-color">
+          <a href="#" class="brand-logo right">SPairing</a>
+          <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+          <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Sing In</Link></li>
+            <li><Link to="/register">Sing In</Link></li>
+          </ul>
+          <ul class="side-nav" id="mobile-demo">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Sing In</Link></li>
+            <li><Link to="/register">Sing In</Link></li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
 
 export default Header
