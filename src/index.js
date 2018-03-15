d9f1a8cdacfe1fd17264d6f885d0cfae7b1f08b3
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-03-14 21:50:11 -0500

    added router schema

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './styles/css/App.css'
 
 import 'materialize-css'
 import 'materialize-css/dist/css/materialize.min.css'
-
-import App from './components/App'
+import Routes from './js/routes'
 import registerServiceWorker from './js/registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <Routes/>
+  </Router>,
+  document.getElementById('root'))
 registerServiceWorker()
