7b45e26bfe5c9f7f19b03b25194eb7c7fe858587
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-04-18 21:34:53 -0500

    Refactored

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,7 @@
+/* global gapi */
 import React, { Component } from 'react'
-import { GoogleLogin } from 'react-google-login';
-// import axios from 'axios'
+import { GoogleLogin } from 'react-google-login'
+import axios from 'axios'
 
 //Assets
 import store from '../js/store'
@@ -15,29 +16,9 @@ class SignIn extends Component {
   }
 
   handleSubmit(event) {
-    // const regUser = /^.+@.+\..+$/
-    // const regPassword = /[a-z0-9]+$/
     event.preventDefault()
     const email = document.getElementById('email').value
     const password = document.getElementById('password').value
-
-    // var matchEmail = username.match(regUser)
-    // var matchPassword = password.match(regPassword)
-    //
-    // this.setState({usernameState: matchEmail && matchEmail.index === 0})
-    // this.setState({passwordState: matchPassword && matchPassword.index === 0})
-
-    // if(this.usernameState && this.passwordState){
-    //   this.setState({
-    //     error: {},
-    //     isLoading: true
-    //   })
-    //
-    //   this.props.login(this.state).then(
-    //     (res) => this.context.history.router.push('/'),
-    //     (err) => this.setState({errors:err.data.errors, isLoading:false})
-    //   )
-    // }
 
     const credentials = {
       "auth": {
@@ -61,38 +42,24 @@ class SignIn extends Component {
 
   responseGoogle = (response) => {
     const data = {id_token: response.Zi.id_token }
+    axios({
+      url: '/google_authentication',
+      method: 'post',
+      baseURL: 'http://localhost:3001',
+      headers: { 'Content-Type': 'application/json' },
+      data
+    }).then(
+    (res) => {
+      if (res.data.jwt) {
+        localStorage.setItem('spToken', res.data.jwt)
+        store.dispatch(login())
+      } else {
+        //La contraseña es incorrecta
 
-    // axios({
-    //   url: '/google_authentication',
-    //   method: 'post',
-    //   baseURL: 'http://localhost:3001',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   data
-    // }).then(
-    // (res) => {
-    //   if (res.data.jwt) {
-    //     localStorage.setItem('spToken', res.data.jwt)
-    //     store.dispatch(login())
-    //   } else {
-    //     //La contraseña es incorrecta
-    //
-    //     //No esta registrado
-    //   }
-    // })
-
-    POST('/google_authentication', data).then(
-      (res) => {
-        if (res.data.jwt) {
-          localStorage.setItem('spToken', res.data.jwt)
-          store.dispatch(login())
-        } else {
-          //La contraseña es incorrecta
-
-          //No esta registrado
-        }
-      })
-
-    }
+        //No esta registrado
+      }
+    })
+  }
 
   render() {
     return (
@@ -111,14 +78,12 @@ class SignIn extends Component {
                   <label htmlFor="password">Password</label>
                   <input type="password" id="password"/>
                 </div>
-                <button className="waves-effect waves-light btn primary-color">Sign In</button>
+                <button className="waves-effect waves-light btn primary-color" style={{"marginBottom": "20px"}}>Sign In</button>
                 <GoogleLogin
-                  clientId="683912365974-b6fhgbk93ae2vomb7rejqgl6ea5l4bli.apps.googleusercontent.com"
-                  buttonText="Sign in with Google"
-                  onSuccess={this.responseGoogle}
-                  onFailure={this.responseGoogle}
+                    clientId="toca poner la env"
+                    buttonText="Sign in with Google"
+                    onSuccess={this.responseGoogle}
                 />
-
                 <h6><br/>
                   <a href="/resetpassword">Forgot your password?</a>
                 </h6>
