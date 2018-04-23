171c58bc6ac7fc5520a2649a38bde8c02295f15c
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-04-23 00:37:25 -0500

    refactored

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -56,15 +56,11 @@ class SignIn extends Component {
   responseGoogle = (response) => {
     this.setState({ logginIn: true })
     const data = {id_token: response.Zi.id_token }
-    axios({
-      url: '/google_authentication',
-      method: 'post',
-      baseURL: BASE_URL,
-      headers: { 'Content-Type': 'application/json' },
-      data
-    }).then(
+
+    POST('/google_authentication', data).then(
       (res) => {
           localStorage.setItem('spToken', res.data.jwt)
+          localStorage.setItem('userId', res.data.user_id)
           store.dispatch(login())
       }
     ).catch(
