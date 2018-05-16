68a383084bb8bbdc3c87a40b672aab0893a49df8
Raul Coral <rfcoralj@unal.edu.co>
2018-05-16 20:57:23 +0000

    Doing friendAction

--- src/components/profile/FriendAction.js
+++ src/components/profile/FriendAction.js
@@ -1,41 +1,45 @@
 import React, { Component } from 'react'
 
 // Assets
-import { GET_AUTH } from '../../js/assets'
+import { GET_AUTH } from '../../js/requests'
 
 class FriendAction extends Component {
-  constructor(props) {
-    super(props)
-  }
 
-  addFriend(){
-    GET_AUTH(`/friend_request/:receiver_id`).then(
+  addFriend(event, friendId){
+    event.preventDefault()
+    
+    GET_AUTH(`/friend_request/${friendId}`).then(
       res => {
-
+        console.log(res.data)
       }
     ).catch(
       error => {
-
+        console.log(error)
       }
     )
 
   }
 
-  removeFriend(){
-    GET_AUTH(`/remove_friend/:receiver_id`).then(
+  removeFriend(event, friendId){
+    event.preventDefault()
+    GET_AUTH(`/remove_friend/${friendId}`).then(
       res => {
-
+        console.log(res.data)
       }
     ).catch(
       error => {
-        
+        console.log(error)
       }
     )
   }
-
+  
   render(){
+    const { areFriends, friendId } = this.props
     return (
-
+      <button className="waves-effect waves-light btn primary-color"
+        onClick={ (e) => (areFriends) ? this.removeFriend(e, friendId) : this.addFriend(e, friendId) } >
+        { (areFriends) ? "Remove Friend" : "Add Friend" }
+      </button>
     )
   }
 }
